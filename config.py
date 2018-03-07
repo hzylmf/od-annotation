592fe252cae7b03021160a0d3caa42c7f520a417
hzylmf <hzylmf@foxmail.com>
2018-03-07 19:23:24 +0800

    Update config.py

--- config.py
+++ config.py
@@ -6,13 +6,6 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 # Flask
 DEBUG = True
 
-# 数据库配置
-MYSQL_HOST = "h136"
-MYSQL_PORT = 3306
-MYSQL_USER = "mynah"
-MYSQL_PASS = "123qdcz$%^"
-MYSQL_DB = "ticket"
-
 # 日志配置
 LOG_DIR = os.path.join(basedir, 'logs')
 LOG_FORMAT = '%(asctime)s %(levelname)s: %(message)s [in %(module)s.%(funcName)s:%(lineno)d]'
